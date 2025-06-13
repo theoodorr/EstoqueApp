a6ba57a3777dfe7a80025b3d80e3a5ced6060da0
Lucas Moraes <lucasmoraes016@gmail.com>
2025-06-12 21:49:07 -0300

    ADDED: Produto model.

---
+++ Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace EstoqueApp.Models;
+
+public class Produto
+{
+    public string Name;
+
+    public string Code;
+
+    public float Price;
+
+    public int Quantity;
+
+    public Produto(string name)
+    {
+        Name = name;
+        Code = "0";
+        Price = 0;
+        Quantity = 1;
+    }
+}
