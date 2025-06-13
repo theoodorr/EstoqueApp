bc7a22a7832389b308c23c6b0c7514c501ff38b7
Lucas Moraes <lucasmoraes016@gmail.com>
2025-06-12 21:48:03 -0300

    ADDED: Service to handle Produtos.

---
+++ Services/ProdutosHandler.cs
@@ -0,0 +1,28 @@
+﻿using EstoqueApp.Models;
+
+namespace EstoqueApp.Services;
+
+public class ProdutosHandler
+{
+    private List<Produto> produtos;
+
+
+    public ProdutosHandler()
+    {
+        produtos = new()
+        {
+            new("Maça"){Code = "1", Price = 0.5f, Quantity = 3},
+            new("Banana"){Code = "2", Price = 1.0f, Quantity = 5}
+        };
+    }
+
+    public void AddProduto(Produto produto)
+    {
+        produtos.Add(produto);
+    }
+
+    public List<Produto> GetProdutos()
+    {
+        return produtos;
+    }
+}
